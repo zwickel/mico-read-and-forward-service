6437d60cfc7163d2b5402241d7fa336ddd5359a8
ma <mariusaltmann@outlook.de>
2021-05-05 11:46:01 +0200

    rename app file

--- src/main/java/io/github/ust/mico/readandforward/ReadAndForwardApplication.java
+++ src/main/java/io/github/ust/mico/readandforward/ReadAndForwardApplication.java
@@ -2,9 +2,6 @@ package io.github.ust.mico.readandforward;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ReadAndForwardApplication {
